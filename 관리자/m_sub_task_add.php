61eeca37b31bce6c8b198369414a51a972648096
Jejun310 <jejunna@naver.com>
2024-11-24 02:01:28 +0900

    Update m_sub_task_add.php

--- 관리자/m_sub_task_add.php
+++ 관리자/m_sub_task_add.php
@@ -3,7 +3,7 @@ session_start();
 include 'db.php';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
