cdb2e4ae55aa9701f607e6e0899bbbff97e7bad7
syyy12 <139008659+syyy12@users.noreply.github.com>
2024-11-23 19:17:46 +0900

    Update process_project_edit.php

--- 관리자/process_project_edit.php
+++ 관리자/process_project_edit.php
@@ -1,102 +1,105 @@
 <?php
-# 히스토리에 저장하고 원래 테이블 저장하고 테이블 수정까지 하는 2024 11 23
 session_start();
 include 'db.php';
 
 // POST 데이터 수신
 $project_id = $_POST['project_id'];
 
-// 1. 프로젝트 업데이트
-$project_name = mysqli_real_escape_string($conn, $_POST['project_name']);
-$description = mysqli_real_escape_string($conn, $_POST['description']);
-$start = isset($_POST['start']) ? mysqli_real_escape_string($conn, $_POST['start']) : null;
-$end = isset($_POST['end']) ? mysqli_real_escape_string($conn, $_POST['end']) : null;
+// 데이터베이스 트랜잭션 시작
+mysqli_begin_transaction($conn, MYSQLI_TRANS_START_READ_WRITE);
 
-$updateProjectQuery = "
-    UPDATE project 
-    SET project_name = '$project_name', 
-        description = '$description', 
-        start = '$start', 
-        end = '$end' 
-    WHERE id = $project_id
-";
+try {
+    // 1. 프로젝트 업데이트
+    $project_name = mysqli_real_escape_string($conn, $_POST['project_name']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $start = isset($_POST['start']) ? mysqli_real_escape_string($conn, $_POST['start']) : null;
+    $end = isset($_POST['end']) ? mysqli_real_escape_string($conn, $_POST['end']) : null;
 
-$projectResult = mysqli_query($conn, $updateProjectQuery);
+    $updateProjectQuery = "
+        UPDATE project 
+        SET project_name = '$project_name', 
+            description = '$description', 
+            start = '$start', 
+            end = '$end' 
+        WHERE id = $project_id
+    ";
 
-if (!$projectResult) {
-echo $updateProjectQuery;
+    if (!mysqli_query($conn, $updateProjectQuery)) {
+        throw new Exception("프로젝트 업데이트 실패: " . mysqli_error($conn));
+    }
 
-    die("프로젝트 업데이트 실패: " . mysqli_error($conn));
-}
+    // 2. 테스크 업데이트 및 삭제
+    if (!empty($_POST['tasks']) && is_array($_POST['tasks'])) {
+        foreach ($_POST['tasks'] as $task_id => $task) {
+            if (isset($task['is_deleted']) && $task['is_deleted'] == 1) {
+                // 테스크 삭제
+                $deleteTaskQuery = "DELETE FROM task WHERE id = $task_id";
+                if (!mysqli_query($conn, $deleteTaskQuery)) {
+                    throw new Exception("테스크 삭제 실패: " . mysqli_error($conn));
+                }
+            } else {
+                // 테스크 업데이트
+                $task_name = mysqli_real_escape_string($conn, $task['task_name']);
+                $description = mysqli_real_escape_string($conn, $task['description']);
+                $start = mysqli_real_escape_string($conn, $task['start']);
+                $end = mysqli_real_escape_string($conn, $task['end']);
 
-// 2. 테스크 업데이트
-if (!empty($_POST['tasks']) && is_array($_POST['tasks'])) {
-    foreach ($_POST['tasks'] as $task_id => $task) {
-        if (isset($task['is_deleted']) && $task['is_deleted'] == 1) {
-            // 테스크 삭제
-            $deleteTaskQuery = "DELETE FROM task WHERE id = $task_id";
-            if (!mysqli_query($conn, $deleteTaskQuery)) {
-                die("테스크 삭제 실패: " . mysqli_error($conn));
-            }
-        } else {
-            // 테스크 업데이트
-            $task_name = mysqli_real_escape_string($conn, $task['task_name']);
-            $description = mysqli_real_escape_string($conn, $task['description']);
-            $start = mysqli_real_escape_string($conn, $task['start']);
-            $end = mysqli_real_escape_string($conn, $task['end']);
-
-            $updateTaskQuery = "
-                UPDATE task 
-                SET task_name = '$task_name', 
-                    description = '$description', 
-                    start = '$start', 
-                    end = '$end' 
-                WHERE id = $task_id
-            ";
-            if (!mysqli_query($conn, $updateTaskQuery)) {
-                die("테스크 업데이트 실패: " . mysqli_error($conn));
+                $updateTaskQuery = "
+                    UPDATE task 
+                    SET task_name = '$task_name', 
+                        description = '$description', 
+                        start = '$start', 
+                        end = '$end' 
+                    WHERE id = $task_id
+                ";
+                if (!mysqli_query($conn, $updateTaskQuery)) {
+                    throw new Exception("테스크 업데이트 실패: " . mysqli_error($conn));
+                }
             }
         }
     }
-} else {
-    // 테스크가 없을 경우 처리
-    echo "테스크가 없습니다.<br>";
-}
 
-// 3. 서브 테스크 업데이트
-if (!empty($_POST['sub_tasks']) && is_array($_POST['sub_tasks'])) {
-    foreach ($_POST['sub_tasks'] as $sub_task_id => $sub_task) {
-        if (isset($sub_task['is_deleted']) && $sub_task['is_deleted'] == 1) {
-            // 서브 테스크 삭제
-            $deleteSubTaskQuery = "DELETE FROM sub_task WHERE id = $sub_task_id";
-            if (!mysqli_query($conn, $deleteSubTaskQuery)) {
-                die("서브 테스크 삭제 실패: " . mysqli_error($conn));
-            }
-        } else {
-            // 서브 테스크 업데이트
-            $sub_task_name = mysqli_real_escape_string($conn, $sub_task['sub_task_name']);
-            $description = mysqli_real_escape_string($conn, $sub_task['description']);
-            $start = mysqli_real_escape_string($conn, $sub_task['start']);
-            $end = mysqli_real_escape_string($conn, $sub_task['end']);
+    // 3. 서브 테스크 업데이트 및 삭제
+    if (!empty($_POST['sub_tasks']) && is_array($_POST['sub_tasks'])) {
+        foreach ($_POST['sub_tasks'] as $sub_task_id => $sub_task) {
+            if (isset($sub_task['is_deleted']) && $sub_task['is_deleted'] == 1) {
+                // 서브 테스크 삭제
+                $deleteSubTaskQuery = "DELETE FROM sub_task WHERE id = $sub_task_id";
+                if (!mysqli_query($conn, $deleteSubTaskQuery)) {
+                    throw new Exception("서브 테스크 삭제 실패: " . mysqli_error($conn));
+                }
+            } else {
+                // 서브 테스크 업데이트
+                $sub_task_name = mysqli_real_escape_string($conn, $sub_task['sub_task_name']);
+                $description = mysqli_real_escape_string($conn, $sub_task['description']);
+                $start = mysqli_real_escape_string($conn, $sub_task['start']);
+                $end = mysqli_real_escape_string($conn, $sub_task['end']);
 
-            $updateSubTaskQuery = "
-                UPDATE sub_task 
-                SET sub_task_name = '$sub_task_name', 
-                    description = '$description', 
-                    start = '$start', 
-                    end = '$end' 
-                WHERE id = $sub_task_id
-            ";
-            if (!mysqli_query($conn, $updateSubTaskQuery)) {
-                die("서브 테스크 업데이트 실패: " . mysqli_error($conn));
+                $updateSubTaskQuery = "
+                    UPDATE sub_task 
+                    SET sub_task_name = '$sub_task_name', 
+                        description = '$description', 
+                        start = '$start', 
+                        end = '$end' 
+                    WHERE id = $sub_task_id
+                ";
+                if (!mysqli_query($conn, $updateSubTaskQuery)) {
+echo "실행된 쿼리: " . $updateSubTaskQuery;
+
+                    throw new Exception("서브 테스크 업데이트 실패: " . mysqli_error($conn));
+                }
             }
         }
     }
-} else {
-    // 서브 테스크가 없을 경우 처리
-    echo "서브 테스크가 없습니다.<br>";
-}
 
+    // 모든 작업 성공 시 커밋
+    mysqli_commit($conn);
+
+} catch (Exception $e) {
+    // 실패 시 롤백
+    mysqli_rollback($conn);
+    die($e->getMessage());
+}
 
 // 연결 종료
 mysqli_close($conn);
