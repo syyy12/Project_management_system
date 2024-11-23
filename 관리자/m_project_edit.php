b1424c75d82ce5bf9c0daf5748ae0a94ef2d5394
ohohtani <138994924+ohohtani@users.noreply.github.com>
2024-11-24 00:42:23 +0900

    Update m_project_edit.php

--- 관리자/m_project_edit.php
+++ 관리자/m_project_edit.php
@@ -125,3 +125,94 @@ $subTasks = $subTaskStmt->get_result()->fetch_all(MYSQLI_ASSOC);
     </FORM>
 </BODY>
 </HTML>
+
+// Sub-task 데이터 가져오기
+$subTaskQuery = "
+    SELECT id, task_id, sub_task_name, start, end, min_days, description, is_completed
+    FROM sub_task
+    WHERE task_id IN (SELECT id FROM task WHERE project_id = ?)
+";
+$subTaskStmt = $conn->prepare($subTaskQuery);
+$subTaskStmt->bind_param("i", $project_id);
+$subTaskStmt->execute();
+$subTasks = $subTaskStmt->get_result()->fetch_all(MYSQLI_ASSOC);
+?>
+
+<HTML>
+<HEAD>
+<META http-equiv="content-type" content="text/html; charset=utf-8">
+</HEAD>
+<BODY>
+    <h1>프로젝트 수정</h1>
+    <FORM METHOD="post" ACTION="process_project_edit.php">
+<!-- 숨겨진 프로젝트 ID 전달 -->
+    <input type="hidden" name="project_id" value="<?php echo $project_id; ?>">
+
+        <h3>프로젝트 정보</h3>
+        <label>프로젝트 이름:</label>
+        <INPUT TYPE="text" NAME="project_name" VALUE="<?php echo htmlspecialchars($project['project_name']); ?>"><br>
+        
+        <label>설명:</label>
+        <INPUT TYPE="text" NAME="description" VALUE="<?php echo htmlspecialchars($project['description']); ?>"><br>
+        
+       <label>시작 날짜:</label>
+<input type="date" name="start" value="<?php echo $project['start']; ?>"><br>
+
+<label>종료 날짜:</label>
+<input type="date" name="end" value="<?php echo $project['end']; ?>"><br>
+
+        
+<h3>테스크 수정</h3>
+<?php foreach ($tasks as $task): ?>
+    <label>Task 이름:</label>
+    <INPUT TYPE="text" NAME="tasks[<?php echo $task['id']; ?>][task_name]" VALUE="<?php echo htmlspecialchars($task['task_name']); ?>"><br>
+    
+    <label>설명:</label>
+    <INPUT TYPE="text" NAME="tasks[<?php echo $task['id']; ?>][description]" VALUE="<?php echo htmlspecialchars($task['description']); ?>"><br>
+
+    <label>시작 날짜:</label>
+    <INPUT TYPE="date" NAME="tasks[<?php echo $task['id']; ?>][start]" VALUE="<?php echo htmlspecialchars($task['start']); ?>"><br>
+
+    <label>종료 날짜:</label>
+    <INPUT TYPE="date" NAME="tasks[<?php echo $task['id']; ?>][end]" VALUE="<?php echo htmlspecialchars($task['end']); ?>"><br>
+
+    <!-- 삭제 체크박스 -->
+    <label>
+        <input type="checkbox" name="tasks[<?php echo $task['id']; ?>][is_deleted]" value="1">
+        삭제
+    </label>
+    <br><br>
+<?php endforeach; ?>
+
+<h3>서브 테스크 수정</h3>
+<?php foreach ($subTasks as $subTask): ?>
+  <label>Sub-task 이름:</label>
+    <input type="text" name="sub_tasks[<?php echo $subTask['id']; ?>][sub_task_name]" 
+           value="<?php echo htmlspecialchars($subTask['sub_task_name']); ?>"><br>
+    
+    <label>설명:</label>
+    <input type="text" name="sub_tasks[<?php echo $subTask['id']; ?>][description]" 
+           value="<?php echo htmlspecialchars($subTask['description']); ?>"><br>
+
+    <label>시작 날짜:</label>
+    <input type="date" name="sub_tasks[<?php echo $subTask['id']; ?>][start]" 
+           value="<?php echo htmlspecialchars($subTask['start']); ?>"><br>
+
+    <label>종료 날짜:</label>
+    <input type="date" name="sub_tasks[<?php echo $subTask['id']; ?>][end]" 
+           value="<?php echo htmlspecialchars($subTask['end']); ?>"><br>
+
+    <!-- 삭제 체크박스 -->
+    <label>
+        <input type="checkbox" name="sub_tasks[<?php echo $subTask['id']; ?>][is_deleted]" value="1">
+        삭제
+    </label>
+    <br><br>
+<?php endforeach; ?>
+
+
+        
+        <INPUT TYPE="submit" VALUE="저장">
+    </FORM>
+</BODY>
+</HTML>
